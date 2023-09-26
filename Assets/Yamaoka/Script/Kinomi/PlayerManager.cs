3347fed706d640eabea7b50f053c976a87cc1aa6
kiyosumi874 <shiiharakiyosumi@gmial.com>
2023-09-26 16:28:51 +0900

    木の実を使用する際の処理の仮作成

---
+++ Assets/Yamaoka/Script/Kinomi/PlayerManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<KinomiData> kinomiDataList = new List<KinomiData>();   // プレイヤーの所持木の実
+
+    /// <summary>
+    /// 木の実を取得
+    /// </summary>
+    /// <param name="kinomiID">木の実のID</param>
+    /// <param name="count">取得する個数</param>
+    public void CountKinomi(int kinomiID, int count)
+    {
+        // List内を検索
+        for(int i = 0; i < kinomiDataList.Count; i++)
+        {
+            // IDが一致していたらカウント
+            if (kinomiDataList[i].id == kinomiID)
+            {
+                kinomiDataList[i].CountUp(count);
+                break;
+            }
+        }
+
+        // IDが一致しなければアイテムを追加
+        KinomiData kinomiData = new KinomiData(kinomiID, count);
+        kinomiDataList.Add(kinomiData);
+    }
+
+    /// <summary>
+    /// 木の実をロスト
+    /// </summary>
+    /// <param name="kinomiID">木の実のID</param>
+    /// <param name="count">ロストする個数</param>
+    public void LostKinomi(int kinomiID, int count)
+    {
+        // List内を検索
+        for (int i = 0; i < kinomiDataList.Count; i++)
+        {
+            // IDが一致していたらカウント
+            if (kinomiDataList[i].id == kinomiID)
+            {
+                kinomiDataList[i].CountDown(count);
+                break;
+            }
+        }
+    }
+
+}
