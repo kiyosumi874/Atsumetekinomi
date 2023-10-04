ce2433f47d215178d8a2f7819bcb23778054b97a
FCA <test.test>
2023-10-04 13:07:10 +0900

    PlayerControllerの更新

--- Assets/Yamaoka/Script/PlayerController.cs
+++ Assets/Yamaoka/Script/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     private Vector3 inputAxis;
     public float moveForceMultiplier;    // 移動速度の入力に対する追従度
 
+    private Vector3 moveDirection;   // 進行方向
+
     private void Start()
     {
         this.rb = GetComponent<Rigidbody>();
@@ -24,5 +26,9 @@ public class PlayerController : MonoBehaviour
     {
         inputAxis.Normalize();
         rb.AddForce(inputAxis * -moveSpeed * moveForceMultiplier);
+
+        // 進行方向に回転させる
+        moveDirection = -inputAxis;
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * 10.0f);
     }
 }
