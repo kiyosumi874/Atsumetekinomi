6be0ad432d3a8a1735ba31621c142b38420085aa
YamaokaShinsaku <argento130613@gmail.com>
2023-10-06 22:40:53 +0900

    GPUを使用して雪を降らせるシェーダーを作成開始

---
+++ Assets/Yamaoka/Script/Snow.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// GPUを使用し、雪を降らせる
+/// </summary>
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Snow : MonoBehaviour
+{
+    // 頂点、その他を用意
+    // 1ドローコールで65000頂点扱えるので、一粒4頂点の雪はその1/4まで出せる
+    const int SNOW_NUM = 8000;  // とりあえず 1/8個出す
+    private Vector3[] vertices; // 頂点
+    private int[] triangles;    // 頂点を結んだ三角形
+    private Color[] colors;
+    private Vector2[] uvs;      // UV座標
+
+    private float range;    // 雪を降らせる範囲
+    private float rangeR;
+    private Vector3 move = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 範囲内のランダムな点に、4頂点をまとめる
+        range = 16f;
+        rangeR = 1.0f / range;
+        vertices = new Vector3[SNOW_NUM * 4];
+        for(var i = 0; i < SNOW_NUM; ++i)
+        {
+            float x = Random.Range(-range, range);
+            float y = Random.Range(-range, range);
+            float z = Random.Range(-range, range);
+
+            var point = new Vector3(x, y, z);
+            vertices[i * 4 + 0] = point;
+            vertices[i * 4 + 1] = point;
+            vertices[i * 4 + 2] = point;
+            vertices[i * 4 + 3] = point;
+        }
+
+        // 4頂点を矩形にするように結んで面にする(三角形2つずつ)
+        triangles = new int[SNOW_NUM * 6];
+        for(int i = 0; i < SNOW_NUM; ++i)
+        {
+            triangles[i * 6 + 0] = i * 4 + 0;
+            triangles[i * 6 + 1] = i * 4 + 1;
+            triangles[i * 6 + 2] = i * 4 + 2;
+
+            triangles[i * 6 + 3] = i * 4 + 2;
+            triangles[i * 6 + 4] = i * 4 + 1;
+            triangles[i * 6 + 5] = i * 4 + 3;
+        }
+
+        // UV座標(今回はテクスチャ座標以外にも使用する)
+        uvs = new Vector2[SNOW_NUM * 4];
+        for(int i = 0; i < SNOW_NUM; ++i)
+        {
+            uvs[i * 4 + 0] = new Vector2(0f, 0f);
+            uvs[i * 4 + 1] = new Vector2(1f, 0f);
+            uvs[i * 4 + 2] = new Vector2(0f, 1f);
+            uvs[i * 4 + 3] = new Vector2(1f, 1f);
+        }
+
+        // メッシュに格納する
+        Mesh mesh = new Mesh();
+        mesh.name = "MeshSnowFlakes";
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.colors = colors;
+        mesh.uv = uvs;
+        // Mesh.bounds は、メッシュの境界体積を表す構造体 -> メッシュ自体の空間内の軸に沿ったバウンディングボックス
+        // (Bounding Box -> 画像や映像の中の物体を囲んだ部分領域)
+        // Frustom Cullingされないために、boundsに 99999999 を入れている
+        mesh.bounds = new Bounds(Vector3.zero, Vector3.one * 99999999);
+        var mf = GetComponent<MeshFilter>();
+        mf.sharedMesh = mesh;
+    }
+
+    /// <summary>
+    /// transformの更新後に呼ばれてほしいので、LateUpdate
+    /// </summary>
+    void LateUpdate()
+    { 
+        // カメラの前方Ⅰを計算
+        // ここを中心に雪が降る
+        var targetPosition = Camera.main.transform.TransformPoint(Vector3.forward * range);
+        var mr = GetComponent<Renderer>();
+        // シェーダー定数の設定
+        mr.material.SetFloat("_Range", range);
+        mr.material.SetFloat("_RangeR", rangeR);
+        mr.material.SetFloat("_Size", 0.1f);
+        mr.material.SetVector("_MoveTotal", move);
+        mr.material.SetVector("_CamUp", Camera.main.transform.up);
+        mr.material.SetVector("_TargetPosition", targetPosition);
+    }
+}
---
+++ Assets/Yamaoka/Shader/Snow/Snow.shader
@@ -0,0 +1,93 @@
+// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Custom/Snow"
+{
+    Properties
+    {
+        _MainTex ("Base (RGB)", 2D) = "white" {}
+    }
+    SubShader
+    {
+        // 透明パスに描く
+        Tags { "Queue"="Transparent" "IgnoreProjector"="True" "RenderType"="Transparent"}
+        ZWrite Off  // Zは描かない
+        Cull Off    // backface culling をしない
+        Blend SrcAlpha OneMinusSrcAlpha // αブレンディング
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #pragma target 3.0
+
+            #include "UnityCG.cginc"
+
+            uniform sampler2D _MainTex;
+
+            struct appdata_Custom
+            {
+                float4 vertex : POSITION;
+                float2 texcoord : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float4 pos : SV_POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            float4x4 _PrevInMatrix;
+            float3 _TargetPosition;
+            float _Range;
+            float _RangeR;
+            float _Size;
+            float3 _MoveTotal;
+            float3 _CamUp;
+            
+            // 頂点シェーダー
+            v2f vert (appdata_Custom v)
+            {
+                // 常に_TargetPositionの周りに登場するように計算
+                float3 target = _TargetPosition;
+                float3 trip;
+                float3 mv = v.vertex.xyz;
+                mv += _MoveTotal;
+                            // 座標を足すだけはダメ、(頂点を)うまくリピートさせないといけない
+                trip = floor(((target - mv) * _RangeR + 1) * 0.5);
+                trip *= (_Range * 2);
+                mv += trip;
+                
+                // 視線ベクトルを使ってビルボードにする
+                // texcoordを利用して四隅を識別する
+                float3 diff = _CamUp * _Size;
+                float3 finalposition;
+                float3 tv0 = mv;
+                {
+                    float3 eyeVector = ObjSpaceViewDir(float4(tv0, 0));
+                    float3 sideVector = normalize(cross(eyeVector, diff));
+                    tv0 += (v.texcoord.x - 0.5f) * sideVector * _Size;
+                    tv0 += (v.texcoord.y - 0.5f) * diff;
+                    finalposition = tv0;
+                }
+                
+                v2f o;
+                o.pos = UnityObjectToClipPos(float4(finalposition, 1));
+                o.uv = MultiplyUV(UNITY_MATRIX_TEXTURE0, v.texcoord);
+                return o;
+            }
+
+            // フラグメントシェーダー
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // テクスチャを引くだけ
+                return tex2D(_MainTex, i.uv);
+            }
+            ENDCG
+        }
+    }
+}
