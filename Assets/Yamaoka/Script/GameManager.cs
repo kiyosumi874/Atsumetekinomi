f156c972c5059429e609257aad2b33110d73222b
YamaokaShinsaku <argento130613@gmail.com>
2023-10-09 09:27:20 +0900

    ゲーム中に獲得したスコアをGameManagerで受け取れるように修正

--- Assets/Yamaoka/Script/GameManager.cs
+++ Assets/Yamaoka/Script/GameManager.cs
@@ -4,45 +4,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// ゲームの状態を設定
+/// </summary>
 public enum GameState
 {
-    BeforeGame,
-    InGame,
-    AfterGame
+    BeforeGame,     // ゲーム開始前
+    InGame,         // ゲーム中
+    AfterGame       // ゲーム終了
 }
 
 public class GameManager : MonoBehaviour
 {
-    public int countDownMinutes;
-    public float countDownSeconds;
+    // ゲーム開始までのカウントダウン用
     public float gameStartCountDownSeconds;
     [SerializeField]
-    private Text timeText;
-    [SerializeField]
     private Text countDownText;
-    public bool finishCountDown = false;
+    // ゲーム中の制限時間設定用
+    public float countDownSeconds;
+    [SerializeField]
+    private Text timeText;
+
+    //public bool finishCountDown = false;    // カウントダウンが終了したかどうか
 
     [SerializeField]
     GameState gameState = GameState.BeforeGame;
 
-    public int score = 0;
+    public int score = 0;   // 最終スコア
 
     // Start is called before the first frame update
     void Start()
     {
-        //countDownSeconds = countDownMinutes * 60;
-        timeText.enabled = false;
+        //timeText.enabled = false;
         countDownText.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //CountDown(countDownSeconds);
-
         switch(gameState)
         {
             case GameState.BeforeGame:
+                // スコアを初期化する
                 score = 0;
                 GameStartCountDown();
                 break;
@@ -50,20 +53,26 @@ public class GameManager : MonoBehaviour
                 CountDown();
                 break;
             case GameState.AfterGame:
-                //score = ComboManager.instance.score;
                 score = ResultScore();
                 break;
         }
     }
 
+    /// <summary>
+    /// ゲームステータスの変更を行う
+    /// </summary>
+    /// <param name="nextGameState">次のゲームステータス</param>
     public void ChangeGameState(GameState nextGameState)
     {
         gameState = nextGameState;
     }
 
+    /// <summary>
+    /// ゲーム中の時間計測を行う
+    /// </summary>
     public void CountDown()
     {
-        timeText.enabled = true;
+        //timeText.enabled = true;
         countDownSeconds -= Time.deltaTime;
         var span = new TimeSpan(0, 0, (int)countDownSeconds);
         timeText.text = span.ToString(@"mm\:ss");
@@ -76,6 +85,9 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// ゲーム開始前のカウントダウン処理
+    /// </summary>
     public void GameStartCountDown()
     {
         countDownText.enabled = true;
@@ -92,6 +104,10 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// スコアの受け渡しを行う
+    /// </summary>
+    /// <returns>ゲーム中に獲得したスコア</returns>
     public int ResultScore()
     {
         score = ComboManager.instance.score;
