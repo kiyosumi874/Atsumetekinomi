31891f73010aa6a496eeba528d3908313d83ad52
408-19 <408-19>
2023-11-08 12:52:21 +0900

    実行データ書き出しに伴うエラーの修正

--- Assets/Yamaoka/Script/GameManager.cs
+++ Assets/Yamaoka/Script/GameManager.cs
@@ -51,7 +51,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (SceneManager.GetActiveScene().name == "Title")
+        if (SceneManager.GetActiveScene().name == "TitleTest")
         {
             ChangeGameState(GameState.None);
         }
